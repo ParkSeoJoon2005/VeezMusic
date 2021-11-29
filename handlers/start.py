b3cdee4974c022610346a5b41d0844e5975a55f0
Park Seo Joon <87398748+ParkSeoJoon2005@users.noreply.github.com>
2021-11-29 21:12:24 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -48,7 +48,7 @@ async def _human_time_duration(seconds):
 
 
 @Client.on_message(
-    command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited
+    command(["mstart", f"mstart@{BOT_USERNAME}"]) & filters.private & ~filters.edited
 )
 async def start_private(client: Client, message: Message):
     await message.reply_text(
@@ -91,7 +91,7 @@ async def start_private(client: Client, message: Message):
 
 
 @Client.on_message(
-    command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited
+    command(["mstart", f"mstart@{BOT_USERNAME}"]) & filters.group & ~filters.edited
 )
 async def start_group(client: Client, message: Message):
     current_time = datetime.utcnow()
@@ -119,7 +119,7 @@ async def start_group(client: Client, message: Message):
 
 
 @Client.on_message(
-    command(["help", f"help@{BOT_USERNAME}"]) & filters.group & ~filters.edited
+    command(["mhelp", f"mhelp@{BOT_USERNAME}"]) & filters.group & ~filters.edited
 )
 async def help(client: Client, message: Message):
     await message.reply_text(
